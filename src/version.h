b8bce8910adf326f01b080da6446c5bfc325d1fe
infinitecoin <infinitecoin92369@yahoo.com>
2013-09-15 14:59:20 -0700

    modified:   src/version.h

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
 
 // These need to be macro's, as version.cpp's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
-#define CLIENT_VERSION_MINOR       5
+#define CLIENT_VERSION_MINOR       6
 #define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
