16f410e843a1f51b7938a5f86c3380e238371fd7
一路有你 <271833059@qq.com>
2018-09-08 00:26:49 +0800

    fee fixed bug

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       9
 #define CLIENT_VERSION_REVISION    1
-#define CLIENT_VERSION_BUILD       0
+#define CLIENT_VERSION_BUILD       2
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
@@ -37,7 +37,7 @@ static const int PROTOCOL_VERSION = 69004;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 69002;
+static const int MIN_PEER_PROTO_VERSION = 69003;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1199,9 +1199,9 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend, CW
                 int64 nPayFee = nTransactionFee * (1 + (int64)nBytes / 1000);
                 bool fAllowFree = CTransaction::AllowFree(dPriority);
                 int64 nMinFee = wtxNew.GetMinFee(1, fAllowFree, GMF_SEND);
-                if (nFeeRet < max(nPayFee, nMinFee))
+                if (nFeeRet < nMinFee)
                 {
-                    nFeeRet = max(nPayFee, nMinFee);
+                    nFeeRet = nMinFee;
                     continue;
                 }
 
